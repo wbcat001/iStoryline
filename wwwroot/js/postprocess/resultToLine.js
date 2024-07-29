7ca2a650b20b4dda8a5fa99b20585418b7d81f7f
aceron-tech <68225198+aceron-tech@users.noreply.github.com>
2024-07-30 01:13:53 +0900

    ?

---
+++ wwwroot/js/postprocess/resultToLine.js
@@ -0,0 +1,12 @@
+
+
+for (var i=0; i<result["array"].length; i++){
+    var tmp = result["array"][i]
+
+    var id = tmp["name"];
+    var linearray = [];
+    for(var j=0; j<tmp["points"].length; j++){
+        
+    }
+
+}
